2938cf28f0dcc0d925a0f30cb7390bc2c7800c4f
Ponki Phan <ponkiphan@gmail.com>
2024-05-06 12:05:04 +0300

    Routing added, navbar added

--- frontend-lopputyo/src/App.jsx
+++ frontend-lopputyo/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from './components/Home';
+import Training from './components/Training';
+import Customer from './components/Customer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>    
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/training" element={<Training />} /> 
+        <Route path="/customer" element={<Customer />} /> 
+      </Routes>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
---
+++ frontend-lopputyo/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import './App.css';
+
+export default function Navbar() {
+    return (
+        <div className="App">
+            <center>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/customer">Customers</Link></li>
+                    <li><Link to="/training">Trainings</Link></li>
+                </ul>
+            </center>
+        </div>
+    )
+}
