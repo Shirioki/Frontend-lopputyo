8d436f420d2c49d17f4b5e8eadc59d7d0ce738cf
Ponki Phan <ponkiphan@gmail.com>
2024-05-06 12:22:13 +0300

    Customer list done and ag-grid downloaded

--- frontend-lopputyo/src/App.jsx
+++ frontend-lopputyo/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Training from './components/Training';
 import Customer from './components/Customer';
---
+++ frontend-lopputyo/src/components/Customer.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import {AgGridReact} from "ag-grid-react";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+
+export default function Customer() {
+    const [customers, setCustomers] = useState([]);
+
+    const fetchData = () => {
+        fetch('https://customerrestservice-personaltraining.rahtiapp.fi/api/customers')
+            .then(response => response.json())
+            .then(data => {setCustomers(data)})
+            .catch(error => console.log(error));
+    }
+    const [columnDefs] = useState([
+        {field: 'firstname', sortable: true, filter: true},
+        {field: 'lastname', sortable: true, filter: true},
+        {field: 'streetaddress', sortable: true, filter: true},
+        {field: 'postcode', sortable: true, filter: true},
+        {field: 'city', sortable: true, filter: true},
+        {field: 'email', sortable: true, filter: true},
+        {field: 'phone', sortable: true, filter: true},
+    ]);
+
+    return (
+        <>
+        <div className="ag-theme-material" style={{width: 1280, height: 1000}}>
+            <AgGridReact
+                rowData={products}
+                columnDefs={columnDefs}
+                pagination={true}
+                paginationPageSize={10}
+            />
+        </div>
+    </>
+    );
+}
